dbee233909552c9e4affc0a9372d344a5e16c8d2
Rareș Cosma <rares@getbetter.ro>
2017-08-05 20:44:16 +0200

    save no dupes in history

--- lib/history.zsh
+++ lib/history.zsh
@@ -3,8 +3,8 @@ if [ -z "$HISTFILE" ]; then
     HISTFILE=$HOME/.zsh_history
 fi
 
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=50000
+SAVEHIST=50000
 
 # Show history
 case $HIST_STAMPS in
@@ -19,6 +19,7 @@ setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
+setopt hist_save_no_dups
 setopt hist_verify
 setopt inc_append_history
 setopt share_history # share command history data
